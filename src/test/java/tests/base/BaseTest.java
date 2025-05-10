95361d6ab183370464f585b7d4becce95331d56c
Borodich Timofei <borodich.timofei@gmail.com>
2025-05-10 21:36:28 +0300

    fix after method

--- src/test/java/tests/base/BaseTest.java
+++ src/test/java/tests/base/BaseTest.java
@@ -4,10 +4,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.ITestContext;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import steps.GoogleSteps;
+import utils.AllureUtils;
+
+import static utils.AllureUtils.takeScreenshot;
 
 @Listeners(TestListener.class)
 public class BaseTest {
@@ -25,16 +29,17 @@ public class BaseTest {
         options.addArguments("--remote-allow-origins=*");
         options.addArguments("--headless");
         options.addArguments("--start-maximized");
-//        options.addArguments("--disable-extensions");
-//        options.addArguments("--disable-infobars");
         driver = new ChromeDriver(options);
         steps = new GoogleSteps(driver);
         context.setAttribute("driver", driver);
     }
-    
-    @AfterMethod (alwaysRun = true, description = "Closing Browser")
-    public void closeDriver() {
-        if(driver != null) {
+
+    @AfterMethod(alwaysRun = true, description = "Closing Browser")
+    public void closeDriver(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE && driver != null) {
+            takeScreenshot(driver);
+        }
+        if (driver != null) {
             driver.quit();
         }
     }
